c020af76d253feb765019e430d67f5689bd426ad
Evan Tschuy <tschuy@osuosl.org>
2015-04-29 14:41:47 -0700

    Modify tests to reflect new language

--- tests/test.js
+++ tests/test.js
@@ -6,14 +6,14 @@ var app = require('../src/app');
 var request = request_builder.defaults({encoding: null});
 
 describe('GET /', function() {
-  it ('should say typescript', function(done) {
+  it ('should say javascript', function(done) {
     var port = process.env.PORT || 8000;
     request.get('http://localhost:' + port + '/', function(err,
         res, body) {
       var bodyAsString = String.fromCharCode.apply(null, res.body);
       expect(err == null);
       expect(res.statusCode).to.be(200);
-      expect(bodyAsString).to.be('hello typescript');
+      expect(bodyAsString).to.be('hello javascript');
       done();
     });
   });
