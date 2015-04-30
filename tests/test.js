bb8986417a2887e81513f88bfca2c94229cb7d32
Ian Kronquist <iankronquist@teleport.com>
2015-04-30 13:08:05 -0700

    Add tests for /users endpoint

--- tests/test.js
+++ tests/test.js
@@ -20,3 +20,26 @@ describe('GET /', function() {
     });
   });
 });
+
+describe('GET /users', function() {
+  it ('should return all users in the database', function(done) {
+    request.get(base_url + 'users', function(err,
+        res, body) {
+      var bodyAsString = String.fromCharCode.apply(null, res.body);
+      var expected_results = [
+        {
+          "id": 1,
+          "username": "deanj"
+        },
+        {
+          "id": 2,
+          "username": "tschuy"
+        }
+      ];
+      expect(err == null);
+      expect(res.statusCode).to.be(200);
+      expect(JSON.parse(bodyAsString)).to.eql(expected_results);
+      done();
+    });
+  });
+});
