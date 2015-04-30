f4ea5ce4dcdfd693f2e8f4e2b23ad0b46d7ef197
Ian Kronquist <iankronquist@teleport.com>
2015-04-30 13:07:22 -0700

    Put url outside of tests
    The base_url will be the same across all tests.

--- tests/test.js
+++ tests/test.js
@@ -5,10 +5,12 @@ var expect = require('expect.js');
 var app = require('../src/app');
 var request = request_builder.defaults({encoding: null});
 
+var port = process.env.PORT || 8000;
+var base_url = 'http://localhost:' + port + '/';
+
 describe('GET /', function() {
   it ('should say javascript', function(done) {
-    var port = process.env.PORT || 8000;
-    request.get('http://localhost:' + port + '/', function(err,
+    request.get(base_url, function(err,
         res, body) {
       var bodyAsString = String.fromCharCode.apply(null, res.body);
       expect(err == null);
