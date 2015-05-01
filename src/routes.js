ffce8b26956294400f7810dacdcd1fa6b79398aa
Ian Kronquist <iankronquist@teleport.com>
2015-05-01 10:20:17 -0700

    Add remaining GET routes

--- src/routes.js
+++ src/routes.js
@@ -15,6 +15,24 @@ module.exports = function(app) {
     });
   });
 
+  app.get('/projects', function (req, res) {
+    knex('projects').then(function (projects) {
+      return res.send(projects);
+    });
+  });
+
+  app.get('/activities', function (req, res) {
+    knex('activities').then(function (activities) {
+      return res.send(activities);
+    });
+  });
+
+  app.get('/checkins', function (req, res) {
+    knex('checkins').then(function (checkins) {
+      return res.send(checkins);
+    });
+  });
+
   app.post('/users/add', function (req, res) {
     knex('users').insert( {username: req.body.username}).then(function (users) {
       console.log(users);
