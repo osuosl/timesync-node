8d8fbc06807f68082b7d274475c47316cb12a126
Ian Kronquist <iankronquist@teleport.com>
2015-05-01 14:42:13 -0700

    Update functions

--- tests/test.js
+++ tests/test.js
@@ -21,6 +21,10 @@ describe('Endpoints', function (){
       sqlFixtures.create(knexfile.development, test_data).then(function() {
         done();
       });
+    });
+  });
+
+
   afterEach(function(done){
     this.timeout(5000);
     knex('projects').del().then(function() {
@@ -36,121 +40,138 @@ describe('Endpoints', function (){
     });
   });
 
-describe('GET /checkins', function() {
-  it ('should return all checkins in the database', function(done) {
-    request.get(base_url + 'checkins', function(err,
-        res, body) {
-      var bodyAsString = String.fromCharCode.apply(null, res.body);
-      var expected_results = [
-        {
-          "duration": 12,
-          "user": 2,
-          "project": 3,
-          "activity": 2,
-          "notes": "",
-          "issue_uri": "https://github.com/osu-cass/whats-fresh-api/issues/56",
-          "date_worked": null,
-          "created_at": null,
-          "updated_at": null,
-          "id": 1
-        }
-      ];
-      expect(err == null);
-      expect(res.statusCode).to.be(200);
-      expect(JSON.parse(bodyAsString)).to.eql(expected_results);
-      done();
+
+
+  describe('GET /', function() {
+    it ('should say javascript', function(done) {
+      request.get(base_url, function(err,
+          res, body) {
+        var bodyAsString = String.fromCharCode.apply(null, res.body);
+        expect(err == null);
+        expect(res.statusCode).to.be(200);
+        expect(bodyAsString).to.be('hello javascript');
+        done();
+      });
+    });
+  });
+
+  describe('GET /checkins', function() {
+    it ('should return all checkins in the database', function(done) {
+      request.get(base_url + 'checkins', function(err,
+          res, body) {
+        var bodyAsString = String.fromCharCode.apply(null, res.body);
+        var expected_results = [
+          {
+            "duration": 12,
+            "user": 2,
+            "project": 3,
+            "activity": 2,
+            "notes": "",
+            "issue_uri": "https://github.com/osu-cass/whats-fresh-api/issues/56",
+            "date_worked": null,
+            "created_at": null,
+            "updated_at": null,
+            "id": 1
+          }
+        ];
+        expect(err == null);
+        expect(res.statusCode).to.be(200);
+        expect(JSON.parse(bodyAsString)).to.eql(expected_results);
+        done();
+      });
     });
   });
 
 
 
 
-describe('GET /users', function() {
-  it ('should return all users in the database', function(done) {
-    request.get(base_url + 'users', function(err,
-        res, body) {
-      var bodyAsString = String.fromCharCode.apply(null, res.body);
-      var expected_results = [
-        {
-          "id": 1,
-          "username": "deanj"
-        },
-        {
-          "id": 2,
-          "username": "tschuy"
-        }
-      ];
-      expect(err == null);
-      expect(res.statusCode).to.be(200);
-      expect(JSON.parse(bodyAsString)).to.eql(expected_results);
-      done();
+  describe('GET /users', function() {
+    it ('should return all users in the database', function(done) {
+      request.get(base_url + 'users', function(err,
+          res, body) {
+        var bodyAsString = String.fromCharCode.apply(null, res.body);
+        var expected_results = [
+          {
+            "id": 1,
+            "username": "deanj"
+          },
+          {
+            "id": 2,
+            "username": "tschuy"
+          }
+        ];
+        expect(err == null);
+        expect(res.statusCode).to.be(200);
+        expect(JSON.parse(bodyAsString)).to.eql(expected_results);
+        done();
+      });
     });
   });
-});
 
-describe('GET /activities', function() {
-  it ('should return all activities in the database', function(done) {
-    request.get(base_url + 'activities', function(err,
-        res, body) {
-      var bodyAsString = String.fromCharCode.apply(null, res.body);
-      var expected_results = [
-        {
-          "name": "Documentation",
-          "alias": "doc",
-          "id": 1
-        },
-        {
-          "name": "Development",
-          "alias": "dev",
-          "id": 2
-        },
-        {
-          "name": "Systems",
-          "alias": "sys",
-          "id": 3
-        }
-      ];
-      expect(err == null);
-      expect(res.statusCode).to.be(200);
-      expect(JSON.parse(bodyAsString)).to.eql(expected_results);
-      done();
+  describe('GET /activities', function() {
+    it ('should return all activities in the database', function(done) {
+      request.get(base_url + 'activities', function(err,
+          res, body) {
+        var bodyAsString = String.fromCharCode.apply(null, res.body);
+        var expected_results = [
+          {
+            "name": "Documentation",
+            "alias": "doc",
+            "id": 1
+          },
+          {
+            "name": "Development",
+            "alias": "dev",
+            "id": 2
+          },
+          {
+            "name": "Systems",
+            "alias": "sys",
+            "id": 3
+          }
+        ];
+        expect(err == null);
+        expect(res.statusCode).to.be(200);
+        expect(JSON.parse(bodyAsString)).to.eql(expected_results);
+        done();
+      });
     });
   });
-});
 
 
-describe('GET /projects', function() {
-  it ('should return all projects in the database', function(done) {
-    request.get(base_url + 'projects', function(err,
-        res, body) {
-      var bodyAsString = String.fromCharCode.apply(null, res.body);
-      var expected_results = [
-        {
-          "uri": "https://code.osuosl.org/projects/ganeti-webmgr",
-          "name": "Ganeti Web Manager",
-          "alias": "gwm",
-          "owner": 2,
-          "id": 1
-        },
-        {
-          "uri": "https://code.osuosl.org/projects/pgd",
-          "name": "Protein Geometry Database",
-          "alias": "pgd",
-          "owner": 1,
-          "id": 2
-        },
-        {
-          "uri": "https://github.com/osu-cass/whats-fresh-api",
-          "name": "Whats Fresh",
-          "alias": "wf",
-          "owner": 2,
-          "id": 3
-        }
-      ];
-      expect(err == null);
-      expect(res.statusCode).to.be(200);
-      expect(JSON.parse(bodyAsString)).to.eql(expected_results);
-      done();
+  describe('GET /projects', function() {
+    it ('should return all projects in the database', function(done) {
+      request.get(base_url + 'projects', function(err,
+          res, body) {
+        var bodyAsString = String.fromCharCode.apply(null, res.body);
+        var expected_results = [
+          {
+            "uri": "https://code.osuosl.org/projects/ganeti-webmgr",
+            "name": "Ganeti Web Manager",
+            "alias": "gwm",
+            "owner": 2,
+            "id": 1
+          },
+          {
+            "uri": "https://code.osuosl.org/projects/pgd",
+            "name": "Protein Geometry Database",
+            "alias": "pgd",
+            "owner": 1,
+            "id": 2
+          },
+          {
+            "uri": "https://github.com/osu-cass/whats-fresh-api",
+            "name": "Whats Fresh",
+            "alias": "wf",
+            "owner": 2,
+            "id": 3
+          }
+        ];
+        expect(err == null);
+        expect(res.statusCode).to.be(200);
+        expect(JSON.parse(bodyAsString)).to.eql(expected_results);
+        done();
+      });
     });
   });
 });
