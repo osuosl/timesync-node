a51fcf08eb32287583ce340fdc12a88c6b40dc69
Ian Kronquist <iankronquist@teleport.com>
2015-05-01 14:41:27 -0700

    Add before each, after each test setup, takedown

--- tests/test.js
+++ tests/test.js
@@ -1,25 +1,40 @@
-/// <reference path='../typings/tsd.d.ts' />
 var mocha = require('mocha');
 var request_builder = require('request');
 var expect = require('expect.js');
+var sqlFixtures = require('sql-fixtures');
 var app = require('../src/app');
+
 var request = request_builder.defaults({encoding: null});
+var test_data = require('./fixtures/test_data');
+var knexfile = require('../knexfile');
+var knex = require('knex')(knexfile.development);;
 
 var port = process.env.PORT || 8000;
 var base_url = 'http://localhost:' + port + '/';
 
-describe('GET /', function() {
-  it ('should say javascript', function(done) {
-    request.get(base_url, function(err,
-        res, body) {
-      var bodyAsString = String.fromCharCode.apply(null, res.body);
-      expect(err == null);
-      expect(res.statusCode).to.be(200);
-      expect(bodyAsString).to.be('hello javascript');
-      done();
+describe('Endpoints', function (){
+
+  beforeEach(function(done) {
+    this.timeout(5000);
+    // Clear SQLite indexes
+    knex.raw('delete from sqlite_sequence').then(function(resp) {
+      sqlFixtures.create(knexfile.development, test_data).then(function() {
+        done();
+      });
+  afterEach(function(done){
+    this.timeout(5000);
+    knex('projects').del().then(function() {
+      knex('activities').del().then(function() {
+        knex('users').del().then(function() {
+          knex('checkins').del().then(function() {
+            sqlFixtures.destroy().then(function() {
+              done();
+            });
+          });
+        });
+      });
     });
   });
-});
 
 describe('GET /checkins', function() {
   it ('should return all checkins in the database', function(done) {
@@ -46,7 +61,6 @@ describe('GET /checkins', function() {
       done();
     });
   });
-});
 
 
 
