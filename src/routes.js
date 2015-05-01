805258c37120cef7aed52f6486f0a3503adfd1c4
Ian Kronquist <iankronquist@teleport.com>
2015-05-01 11:51:44 -0700

    POST /<endpoint>/add routes for object creation

--- src/routes.js
+++ src/routes.js
@@ -34,8 +34,48 @@ module.exports = function(app) {
   });
 
   app.post('/users/add', function (req, res) {
-    knex('users').insert( {username: req.body.username}).then(function (users) {
-      console.log(users);
+    knex('users').insert(req.body).then(function (user) {
+      console.log(user);
+      knex('users').where(req.body).then(function (user_retrieved) {
+        return res.send(user_retrieved);
+      });
+    }).catch(function (error) {
+      res.send(error);
     });
   });
+
+  app.post('/checkins/add', function (req, res) {
+    knex('checkins').insert(req.body).then(function (checkin) {
+      console.log(checkin);
+      knex('checkins').where(req.body).then(function (checkin_retrieved) {
+        return res.send(checkin_retrieved);
+      });
+    }).catch(function (error) {
+      res.send(error);
+    });
+  });
+
+  app.post('/activities/add', function (req, res) {
+    knex('activities').insert(req.body).then(function (activity) {
+      knex('activities').where(req.body).then(function (activity) {
+        return res.send(activity);
+      });
+    }).catch(function (error) {
+      res.send(error);
+    });
+  });
+
+
+  app.post('/projects/add', function (req, res) {
+    knex('projects').insert(req.body).then(function (project) {
+      console.log(project);
+      knex('projects').where(req.body).then(function (project_retrieved) {
+        return res.send(project_retrieved);
+      });
+    }).catch(function (error) {
+      res.send(error);
+    });
+  });
+
+
 }
