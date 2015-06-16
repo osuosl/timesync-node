51c716dc62b1fd6034974a6394b21b30270f1617
Evan Tschuy <tschuy@osuosl.org>
2015-06-16 14:18:26 -0700

    Change errors.js to return objects

--- src/errors.js
+++ src/errors.js
@@ -1,52 +1,58 @@
-var DATABASE_SAVE_ERROR = 19;
-
 module.exports = {
 
   // strip non-alphanumeric, non-hyphens
-  function createSlugFrom(name) {
+  'createSlugFrom': function(name) {
       return name.toLowerCase()
               .replace(/ /g, '-')
               .replace(/[^\w-]+/g, '');
-  }
+  },
 
-  function errorObjectNotFound(object) {
-      return JSON.stringify({
+  'errorObjectNotFound': function(object) {
+      return {
           'error': "Object not found",
           'errno': 1,
           'text': "Invalid " + object
-      });
-  }
+      };
+  },
 
-  function errorDatabaseSaveFailed(sql_error) {
-      return JSON.stringify({
+  'errorDatabaseSaveFailed': function(sql_error) {
+      return {
           'error': "Database save failed",
           'errno': 2,
           'text': sql_error
-      });
-  }
+      };
+  },
 
-  function errorInvalidForeignKey(object) {
-      return JSON.stringify({
+  'errorInvalidForeignKey': function(object) {
+      return {
           'error': "Invalid foreign key",
           'errno': 3,
           'text': "Invalid " + object
-      });
-  }
+      };
+  },
 
-  function errorNoNameProvided(error) {
-      return JSON.stringify({
+  'errorNoNameProvided': function(error) {
+      return {
           'error': "No Name provided",
           'errno': 4,
           'text': error
-      });
-  }
+      };
+  },
 
-  function errorInvalidValue(error) {
-      return JSON.stringify({
+  'errorInvalidValue': function(error) {
+      return {
           'error': "The provided value wasn't valid",
           'errno': 5,
           'text': error
-      });
+      };
+  },
+
+  'errorInvalidSlug': function(error) {
+      return {
+          'error': "The provided slug wasn't valid",
+          'errno': 6,
+          'text': error
+      };
   }
 
 }
