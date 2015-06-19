8c47405cb2489ae80311f8cb295e15a1d7108906
Tristan Patch <patcht@osuosl.org>
2015-06-18 17:01:21 -0700

    Refactored activity code.

--- src/activities.js
+++ src/activities.js
@@ -1,45 +1,69 @@
 module.exports = function(app) {
-  var knex = app.get('knex');
-  var errors = require('./errors');
+    var knex = app.get('knex');
+    var errors = require('./errors');
 
-  app.get(app.get('version') + '/activities', function (req, res) {
-    knex('activities').then(function (activities) {
-      var count = 0;
-      if (activities.length === 0) {
-        return res.send([]);
-      }
-      activities.forEach(function(activity) {
-        knex('activityslugs').where({'activity': activity.id}).select('name').then(function(slugs) {
-          activity.slugs = [];
-          slugs.forEach(function(slug) {
-            activity.slugs.push(slug.name);
-          });
-          count++;
-          if (count == activities.length) {
-            return res.send(activities);
-          }
-        });
-      });
-    });
-  });
+    app.get(app.get('version') + '/activities', function (req, res) {
 
-  app.get(app.get('version') + '/activities/:slug', function (req, res) {
-    knex('activityslugs').where({'name': req.params.slug}).then(function(activity_slug) {
-      if(activity_slug.length !== 0) {
-        knex('activities').where({'id': activity_slug[0].activity}).then(function (activity_list) {
-          knex('activityslugs').where({'activity': activity_slug[0].activity}).select('name').then(function(slugs) {
-            activity = activity_list[0];
-            activity.slugs = [];
-            slugs.forEach(function(slug) {
-              activity.slugs.push(slug.name);
+        knex('activities').then(function(activities) {
+            knex('activityslugs').then(function(activityslugs) {
+                if (activities.length === 0) {
+                    return res.send([]);
+                }
+
+                var id_activity_map = {};
+                activities.forEach(function(activity) {
+                    activity.slugs = [];
+                    id_activity_map[activity.id] = activity;
+                });
+                activityslugs.forEach(function(slug) {
+                    id_activity_map[slug.activity].slugs.push(slug.name);
+                });
+
+                return res.send(activities);
             });
-            return res.send(activity);
-          });
         });
-      } else {
-        return res.status(404).send(
-          errors.errorInvalidSlug(req.params.slug + " is not a valid activity slug."));
-      }
     });
-  });
+
+    app.get(app.get('version') + '/activities/:slug', function (req, res) {
+
+        /*
+        * Gets an activity and list of slugs from a slug.
+        *
+        * First selects an activity from the name of a slug (from the URI).
+        * Then selects all slug names which match that activity.
+        * Resulting table will look like this:
+        *
+        * +----+-------------+-------------+
+        * | id |     name    |     name    |
+        * +----+-------------+-------------+
+        * |  4 | development |     dev     |
+        * |  4 | development | development |
+        * |  4 | development |    coding   |
+        * |  4 | development | programming |
+        * +----+-------------+-------------+
+        */
+        activitySubquery = knex('activityslugs').select('activity')
+            .where('name', req.params.slug);
+        slugsSubquery = knex('activities').select('id').where('id', '=', activitySubquery);
+
+        knex('activityslugs')
+        .select('activities.id as id', 'activities.name as name', 'activityslugs.name as slug')
+        .where('activity', '=', slugsSubquery)
+        .innerJoin('activities', 'activityslugs.activity', 'activities.id')
+        .then(function(results) {
+
+            if(results.length !== 0) {
+                activity = {id: results[0].id, name: results[0].name, slugs: []};
+
+                results.forEach(function(row) {
+                    activity.slugs.push(row.slug);
+                })
+                res.send(activity);
+            } else {
+                return res.status(404).send(
+                    errors.errorInvalidSlug(req.params.slug + " is not a valid activity slug."));
+            }
+
+        });
+    });
 };
