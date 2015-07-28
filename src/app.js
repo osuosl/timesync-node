eac9167784983af2c3daae6ccfe77ffca6be1d2e
Evan Tschuy <tschuy@osuosl.org>
2015-07-28 12:25:26 -0700

    add "working" user authentication

--- src/app.js
+++ src/app.js
@@ -15,6 +15,56 @@ app.set('knex', knex);
 // Set API version prefix
 app.set('version', '/v1');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bcrypt = require('bcrypt');
+
+var h;
+
+bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash('password', salt, function(err, hash) {
+        console.log('hash: ' + hash);
+        h = hash;
+    });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    knex('users').where({username: username}).then(function(users) {
+      if (!users) {
+        done(null, false, { message: 'Incorrect username.' });
+      }
+      var user = users[0];
+
+      bcrypt.compare(password, h, function(err, res) {
+          if(res) {
+            done(null, user);
+          }
+          else {
+            done(null, false, { message: 'Incorrect password.' });
+          }
+      });
+
+
+    }).catch(
+      function(err) {
+        done(err);
+      }
+    );
+  }
+));
+
 //Load local functions
 require('./routes')(app);
 require('./users')(app);
