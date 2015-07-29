3276b9610d4a42e6ebdd0e639e42c36fefe774b9
Aileen Thai <thai@osuosl.org>
2015-07-29 16:19:38 -0700

    Add password encryption, store to db

--- src/create-account.js
+++ src/create-account.js
@@ -1,38 +1,55 @@
 #!/usr/bin/env node
 
+//Library requirements
 var prompt = require('prompt');
+var bcrypt = require('bcrypt');
+var knexfile = require('../knexfile');
+var db = process.env.DATABASE || 'development';
+
+//Load the database (default = development)
+var knex = require('knex')(knexfile[db]);
 
 
 var info = {
     properties: {
         name: {
             validator: /^[0-9a-zA-Z\-]+$/,
-            message: 'Username can contain only letters, numbers, or dashes',
-            warning: 'Invalid entry',
+            warning: 'Use only letters, numbers, or dashes',
             required: true
         },
         password: {
             //hidden: true,
-            //validator:
-            //message:
             required: true
         }
     }
 };
-
-prompt.start();
-
-prompt.get(info, function (err, result){
-    if (err) { return onErr(err); }
-    console.log('Command-line input received:');
-    console.log('  Username: ' + result.name);
-    console.log('  Password: ' + result.password);
-});
 
 function onErr(err) {
     console.log(err);
     return 1;
 }
 
-//console.log('Arguments:', process.argv.length);  
-//console.log(process.argv);
+prompt.start();
+
+prompt.get(info, function(err, result) {
+    if (err) { return onErr(err); }
+
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(result.password, salt, function(err, hash) {
+            knex('users').insert({username: result.name, password: hash})
+            .then(
+                function() {
+                    console.log('done');
+                })
+            .then(
+                function() {
+                    process.exit(0);
+                });
+        });
+    });
+
+    console.log('Command-line input received:');
+    console.log('  Username: ' + result.name);
+    console.log('  Password: ' + result.password);
+});
+
