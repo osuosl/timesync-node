74c7f2424a81caab13d52c4feefe7c4d8ec9499a
MaraJade <catbookgirl@gmail.com>
2015-07-29 11:17:48 -0700

    Added password and active fields to migrations, updated test fixtures

--- migrations/20150101000000_00.js
+++ migrations/20150101000000_00.js
@@ -22,6 +22,8 @@ exports.up = function(knex, Promise) {
   }).createTable('users', function (table) {
     table.increments('id').primary();
     table.string('username').unique().notNullable();
+    table.string('active').defaultTo(false);
+    table.string('password');
   }).createTable('projectslugs', function (table) {
     table.increments('id').primary();
     table.string('name').unique().notNullable();
