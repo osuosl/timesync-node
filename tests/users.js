120f77ded4f93478a59086db7ecd18dfffd41539
MaraJade <catbookgirl@gmail.com>
2015-07-29 10:34:11 -0700

    Fixed line length issues

--- tests/users.js
+++ tests/users.js
@@ -9,14 +9,18 @@ module.exports = function(expect, request, base_url) {
                         username: 'deanj',
                         active: true,
                         //Hash generated from password 'pass'
+                        //jscs:disable maximumLineLength
                         password: '$2a$10$nkGN8eZN1DbOahNfv8YgWO790imw7poGcjfik1k0zQ9mFqYqtMY0y'
+                        //jscs:enable maximumLineLength
                     },
                     {
                         id: 2,
                         username: 'tschuy',
                         active: false,
                         //Hash generated from password 'password'
+                        //jscs:disable maximumLineLength
                         password: '$2a$10$6jHQo4XTceYyQ/SzgtdhleQqkuy2G27omuIR8MPvSG8rwN4xyaF5W'
+                        //jscs:enable maximumLineLength
                     }
                 ];
                 expect(err).to.be(null);
