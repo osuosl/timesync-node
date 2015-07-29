342cc917022beaf84fbffd71589318218fc39340
Evan Tschuy <tschuy@osuosl.org>
2015-07-29 13:00:37 -0700

    Minor refactor of tests

--- tests/test.js
+++ tests/test.js
@@ -12,7 +12,35 @@ var knex = require('knex')(knexfile.mocha);
 var port = process.env.PORT || 8000;
 var baseUrl = 'http://localhost:' + port + '/v1/';
 
+var reloadFixtures = function(done) {
+    // Clear SQLite indexes
+    knex.raw('delete from sqlite_sequence').then(function() {
+        sqlFixtures.create(knexfile.mocha, testData).then(function() {
+            done();
+        });
+    });
+}
+
+var clearDatabase = function(done) {
+    knex('projects').del().then(function() {
+        knex('activities').del().then(function() {
+            knex('users').del().then(function() {
+                knex('times').del().then(function() {
+                    knex('activityslugs').del().then(function() {
+                        knex('projectslugs').del().then(function() {
+                            sqlFixtures.destroy().then(function() {
+                                done();
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+
 describe('Endpoints', function() {
+    this.timeout(5000);
 
     before(function(done) {
         knex.migrate.latest().then(function() {
@@ -21,13 +49,7 @@ describe('Endpoints', function() {
     });
 
     beforeEach(function(done) {
-        this.timeout(5000);
-        // Clear SQLite indexes
-        knex.raw('delete from sqlite_sequence').then(function() {
-            sqlFixtures.create(knexfile.mocha, testData).then(function() {
-                done();
-            });
-        });
+        reloadFixtures(done);
     });
 
     afterEach(function(done) {
@@ -53,5 +75,24 @@ describe('Endpoints', function() {
     require('./users')(expect, request, baseUrl);
     require('./activities')(expect, request, baseUrl);
     require('./projects')(expect, request, baseUrl);
+
+});
+
+describe('Helpers', function() {
+    this.timeout(5000);
+
+    before(function(done) {
+        knex.migrate.latest().then(function() {
+            done();
+        });
+    });
+
+    beforeEach(function(done) {
+        reloadFixtures(done);
+    });
+
+    afterEach(function(done) {
+        clearDatabase(done);
+    });
 
 });
