12d74047f2fa11ff1b53bc220590a78a60262309
MaraJade <catbookgirl@gmail.com>
2015-07-29 09:32:36 -0700

    Modified user test to contain password and active fields

--- tests/users.js
+++ tests/users.js
@@ -1,16 +1,20 @@
 module.exports = function(expect, request, base_url) {
     describe('GET /users', function() {
-        it('should return all users in the database', function(done) {
+        it('returns all users in the database', function(done) {
             request.get(base_url + 'users', function(err, res) {
                 var bodyAsString = String.fromCharCode.apply(null, res.body);
                 var expected_results = [
                     {
                         id: 1,
-                        username: 'deanj'
+                        username: 'deanj',
+                        active: true,
+                        password: 'pass'
                     },
                     {
                         id: 2,
-                        username: 'tschuy'
+                        username: 'tschuy',
+                        active: false,
+                        password: 'password'
                     }
                 ];
                 expect(err).to.be(null);
