1bcf2d7098443b62f4551d86cec4ef1d7ed0b5f3
ElijahCaine <voigte@osuosl.org>
2015-07-30 15:52:27 -0700

    Added tests for checking datetime
    Code to pass tests does not yet exist.
    fixes #71

--- tests/helpers.js
+++ tests/helpers.js
@@ -3,7 +3,25 @@
 var helper = require('../src/helpers');
 
 module.exports = function(expect) {
-    // This line exists to please the linters and tests
-    // Remove it when you write real tests.
-    expect(helper);
+    describe('checkDateTime helper function', function() {
+        it('Returns Unix Timestamp for valid date', function() {
+            expect(helper.checkDateTime('2015-04-28')
+                         .to.equal(1430179200000));
+        });
+
+        it('Returns Unix Timestamp for valid dateTime', function() {
+            expect(helper.checkDateTime('2015-04-28 16:30')
+                         .to.equal(1430264400000));
+        });
+
+        it('Returns `false` for a non-valid date', function() {
+            expect(helper.checkDateTime('2015-04-82')
+                         .to.equal(false));
+        });
+
+        it('Returns `false` for a non-valid dateTime', function() {
+            expect(helper.checkDateTime('2015-04-28 24:40')
+                         .to.equal(false));
+        });
+    });
 };
