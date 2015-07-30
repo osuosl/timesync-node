0e2130d21aab0c02a8a60f197978a86d407e2248
MaraJade <catbookgirl@gmail.com>
2015-07-30 11:12:25 -0700

    Created activity helper function, keeps ending up undefined

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,25 @@
-module.exports = {
+module.exports = function(app) {
+    var knex = app.get('knex');
+    var errors = require('./errors');
 
-    checkActivities: function(name) {
-        name = null;
-    }
+    //Get all slugs, compare given slug to them
+    function checkActivities(name) {
+        //name = null;
+        var slugs = knex('activityslugs').select('*');
+
+        var valid = 0;
+        for (var i = 0; i < slugs.length; i++){
+            for (var j = 0; j < name.length; i++) {
+                if (name[j] == slugs[i]){
+                    valid += 1;
+                }
+                else if (valid == name.length - 1) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    };
 
 };
