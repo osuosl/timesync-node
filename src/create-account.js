35fe4e5f2bb59bd27642b946f93ca6928bd7b00a
Aileen Thai <thai@osuosl.org>
2015-07-30 10:30:24 -0700

    Limit username length and usable characters

--- src/create-account.js
+++ src/create-account.js
@@ -9,12 +9,12 @@ var db = process.env.DATABASE || 'development';
 //Load the database (default = development)
 var knex = require('knex')(knexfile[db]);
 
-
+//Prompt user for their information
 var info = {
     properties: {
         name: {
-            validator: /^[0-9a-zA-Z\-]+$/,
-            warning: 'Use only letters, numbers, or dashes',
+            validator: /^[0-9\w\-\+\@\+\.]{1,30}$/,
+            warning: 'Use up to 30 alphanumeric, _, @, +, . and - \n',
             required: true
         },
         password: {
@@ -34,13 +34,15 @@ prompt.start();
 prompt.get(info, function(err, result) {
     if (err) { return onErr(err); }
 
+    //Password encryption
     bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(result.password, salt, function(err, hash) {
             knex('users').insert({username: result.name, password: hash})
             .then(
                 function() {
-                    console.log('done');
+                    console.log('\nUser successfully created\n');
                 })
+            //Exits the process after storing user info in the db
             .then(
                 function() {
                     process.exit(0);
@@ -48,7 +50,7 @@ prompt.get(info, function(err, result) {
         });
     });
 
-    console.log('Command-line input received:');
+    console.log('\nCommand-line input received:\n');
     console.log('  Username: ' + result.name);
     console.log('  Password: ' + result.password);
 });
