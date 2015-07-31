3701a00fef52bc01fd5d226f1e615834f70f74bc
Aileen Thai <thai@osuosl.org>
2015-07-31 13:16:31 -0700

    First commit for check username==user
    Refs #57

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // test/helpers.js
 
 var helper = require('../src/helpers');
@@ -6,4 +7,22 @@ module.exports = function(expect) {
     // This line exists to please the linters and tests
     // Remove it when you write real tests.
     expect(helper);
+    // I don't know where any of these parameters are supposed to go .___.
+    describe('checkUser helper function', function() {
+        it('Returns true if username == user',
+            function(done, res, err, username) {
+                var jsonBody = JSON.parse(String.fromCharCode.apply(res.body));
+                var projUser = jsonBody.name;
+                expect(err).to.be(null);
+                expect(username).to.eql(projUser);
+                done();
+            });
+        it('Returns false if username !== user', function(done, res, err) {
+                var jsonBody = JSON.parse(String.fromCharCode.apply(res.body));
+                var projUser = jsonBody.name;
+                expect(err).to.be(null);
+                expect('notauser').to.not.eql(projUser);
+                done();
+        });
+    });
 };
