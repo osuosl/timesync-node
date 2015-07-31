dc63c4a494f48abeea58a7a949cf17f99dfdf337
MaraJade <catbookgirl@gmail.com>
2015-07-31 16:54:26 -0700

    Created helpers file, test thinks function is undefined

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,26 @@
 // src/helpers.js
 
+var app = require('./app');
+var knex = app.get('knex');
+var errors = require('./errors');
+
 module.exports = {
-    checkActivities: function() {}
+    checkActivities: function(names) {
+        var valid = [];
+        for (var i = 0; i < names.length - 1; i++){
+            knex('activities').where({slug:names[i]}).then(function(slug){
+               console.log(slug/* + ' Name: ' + names[i]*/);
+               if (names[i] === slug.name){
+                   valid.append(slug.id);
+                   console.log('Valid slug found for ' + slug);
+               }
+            });
+        }
+        if (valid == names.length - 1){
+            console.log('Valid slugs');
+            //res.send(true);
+            return slug.id;
+        }
+        return false;
+    }
 };
