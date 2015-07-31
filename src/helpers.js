0129de07291ce4f12274f75cc6e64e7ff0752fbf
Evan Tschuy <tschuy@osuosl.org>
2015-07-31 16:48:57 -0700

    lint fix

--- src/helpers.js
+++ src/helpers.js
@@ -56,22 +56,22 @@ module.exports = {
     },
 
     checkProject: function(slug) {
-      // Bluebird promises take a resolve and a reject
-      // essentially, when the data you want is done resolving.
-      // pass it to resolve(). If an error occurs, pass it to
-      // reject().
-      return new Promise(function(resolver, reject) {
-          // get project from database
-          knex('projectslugs').select('project')
-          .where('name', slug).then(function(project) {
-              if(project.length == 0) {
-                  // project doesn't exist -- it could be null, undefined,
-                  // invalid, etc.
-                  reject(slug);
-              } else {
-                  resolver(project[0].project);
-              }
-          }).catch(function(err) { reject(err) });
-      });
+        // Bluebird promises take a resolve and a reject
+        // essentially, when the data you want is done resolving.
+        // pass it to resolve(). If an error occurs, pass it to
+        // reject().
+        return new Promise(function(resolver, reject) {
+            // get project from database
+            knex('projectslugs').select('project')
+            .where('name', slug).then(function(project) {
+                if (project.length === 0) {
+                    // project doesn't exist -- it could be null, undefined,
+                    // invalid, etc.
+                    reject(slug);
+                } else {
+                    resolver(project[0].project);
+                }
+            }).catch(function(err) { reject(err); });
+        });
     }
 };
