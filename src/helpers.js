5c936a7a21ec0655e13fccb2e52d0cee2243d594
Aileen Thai <thai@osuosl.org>
2015-08-03 13:06:42 -0700

    Encounter 'TypeError: undefined is a not function' in tests

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,18 @@
 // src/helpers.js
 
+var app = require('./app');
+//var knex = app.get('knex');
+var errors = require('./errors');
+
 module.exports = {
-    checkProject: function(username) {
-        username = null;
+    checkUser: function(username, objUser) {
+        if (username == objUser) {
+            return true;
+        }
+        if (username !== objUser) {
+            console.log('An error has occurred');
+            return false;
+        }
     }
 };
+
