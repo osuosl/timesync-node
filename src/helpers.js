a981b22fb44c42aa9e79503ac6a08b237f4fbc3e
Evan Tschuy <tschuy@osuosl.org>
2015-08-05 10:16:08 -0700

    Add slug regex checks

--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,33 @@
-// src/helpers.js
-
 var app = require('./app');
 var knex = app.get('knex');
 
 module.exports = {
+    validateSlug: function(slug) {
+        /* matches, in order:
+          1. at least one letter
+          2. any number of alphanumeric
+          3. hyphen-separated sets of alphanumeric
+
+          Valid:
+            test
+            a1b2-c3
+
+          Invalid:
+            1a2b
+            a1b2--c3
+
+          Based on http://stackoverflow.com/a/19256344
+        */
+        if(slug === undefined || slug === null) {
+            // Javascript will cast these to "undefined" and "null",
+            // which then pass the test
+            return false;
+        }
+
+        var isSlug = new RegExp('^[a-z]+[a-z0-9]*(?:-[a-z0-9]+)*$');
+        return isSlug.test(slug);
+    },
+    
     checkUser: function(username, authUser) {
         return new Promise(function(resolve, reject) {
             if (username === authUser) {
@@ -19,4 +43,3 @@ module.exports = {
         });
     }
 };
-
--- tests/helpers.js
+++ tests/helpers.js
@@ -19,4 +19,48 @@ module.exports = function(expect) {
             });
         });
     });
+
+    describe('validateSlug', function() {
+        it('returns true for proper slug', function(done) {
+            expect(helpers.validateSlug('kitten')).to.be.true;
+            done();
+        });
+
+        it('returns true for proper slug with hyphen', function(done) {
+            expect(helpers.validateSlug('kitten-be-cool')).to.be.true;
+            done();
+        });
+
+        it('returns true for proper slug with hyphens and numbers',
+        function(done) {
+            expect(helpers.validateSlug('a1b2-c3')).to.be.true;
+            done();
+        });
+
+        it('returns false for empty string', function(done) {
+            expect(helpers.validateSlug('')).to.be.false;
+            done();
+        });
+
+        it('returns false for null input', function(done) {
+            expect(helpers.validateSlug(null)).to.be.false;
+            done();
+        });
+
+        it('returns false for undefined input', function(done) {
+            expect(helpers.validateSlug(undefined)).to.be.false;
+            done();
+        });
+
+        it('returns false for slugs starting with numbers', function(done) {
+            expect(helpers.validateSlug('123abc')).to.be.false;
+            done();
+        });
+
+        it('returns false for slugs with multiple hyphens in a row',
+        function(done) {
+            expect(helpers.validateSlug('a1b2--c3')).to.be.false;
+            done();
+        });
+    });
 };
