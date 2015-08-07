d3c013896b5959e07ba816346272da8f18b09b0b
Aileen Thai <thai@osuosl.org>
2015-08-07 11:11:17 -0700

    Tests now start with lowercased letters

--- tests/activities.js
+++ tests/activities.js
@@ -87,7 +87,7 @@ module.exports = function(expect, request, baseUrl) {
        be retrieved from the database */
 
     describe('DELETE /activities/:slug', function() {
-        it('Deletes the activity affiliated with the activity slug',
+        it('deletes the activity affiliated with the activity slug',
         function(done) {
             request.del(baseUrl + 'activity/docs', function(err, res) {
                 expect(err).to.be.a('null');
@@ -112,7 +112,7 @@ module.exports = function(expect, request, baseUrl) {
         });
 
         // Checks that delete will fail w/ nonexistent slug
-        it('Fails if it receives a bad slug', function(done) {
+        it('fails if it receives a bad slug', function(done) {
             request.del(baseUrl + 'activities/naps', function(err, res, body) {
                 var jsonBody = JSON.parse(body);
                 var expectedError = {
@@ -129,7 +129,7 @@ module.exports = function(expect, request, baseUrl) {
         });
 
         // Checks that delete will fail w/ invalid slug
-        it('Fails if it receives an invalid slug', function(done) {
+        it('fails if it receives an invalid slug', function(done) {
             request.del(baseUrl + 'activities/###!what',
             function(err, res, body) {
                 var jsonBody = JSON.parse(body);
