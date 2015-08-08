4941c49985212afbed7229f0176ce0d61e2af002
ElijahCaine <voigte@osuosl.org>
2015-08-07 17:01:40 -0700

    Started on times/:id tests
    - Skeleton code for each test exist.
    - I am trying to write a single testing function that I can call so I
    don't duplicate code over ~30 tests (yes, it's going to be almost 30
    tests).  Hopefully that pans out.
    - linter does not pass
    - refs #102

--- tests/times.js
+++ tests/times.js
@@ -957,4 +957,300 @@ module.exports = function(expect, request, baseUrl) {
       });
     });
   });
+
+    describe('POST /times/:id', function() {
+        // The database's entry for `Whats Fresh`'s time entry
+        var originalTime = {
+            duration:    12,
+            user:        "tschuy",
+            project:     ["wf"],
+            notes:       "",
+            activities:  ["docs", "dev"],
+            issue_uri:
+               "https:://github.com/osu-cass/whats-fresh-api/issues/56",
+            date_worked: null,
+            created_at:  null,
+            updated_at:  null,
+            id:          1
+        };
+
+        // A completely patched version of the above time entry
+        // Only contains valid patch elements.
+        var patchedTime = {
+                duration:    15,
+                user:        "deanj",
+                project:     ["pgd"],
+                activities:  ["docs", "sys"],
+                notes:       "Now this is a note",
+                issue_uri:   "https://github.com/osuosl/pgd/pull/19",
+                date_worked: "2015-04-28",
+        };
+
+        // Individual pieces of the above JSON object
+        // Used for sending individual patches
+        patchedTimeDuration   = {duration:    patchedTime.duration};
+        patchedTimeUser       = {user:        patchedTime.user};
+        patchedTimeProject    = {project:     patchedTime.project};
+        patchedTimeActivities = {activities:  patchedTime.activities};
+        patchedTimeNotes      = {notes:       patchedTime.notes};
+        patchedTimeIssueUri   = {issue_uri:   patchedTime.issue_uri};
+        patchedTimeDateWorkd  = {date_worked: patchedTime.date_worked};
+
+        // Sends invalid data to the /times/:id endpoint
+        var invalidTime = {
+                duration:    undefined,
+                user:        undefined,
+                project:     undefined,
+                activities:  undefined,
+                notes:       undefined,
+                issue_uri:   undefined,
+                date_worked: undefined,
+        };
+
+        // Individual pieces of the above JSON object
+        // Used for sending individual patches
+        patchedTimeDuration   = {duration:    invalidTime.duration};
+        patchedTimeUser       = {user:        invalidTime.user};
+        patchedTimeProject    = {project:     invalidTime.project};
+        patchedTimeActivities = {activities:  invalidTime.activities};
+        patchedTimeNotes      = {notes:       invalidTime.notes};
+        patchedTimeIssueUri   = {issue_uri:   invalidTime.issue_uri};
+        patchedTimeDateWorkd  = {date_worked: invalidTime.date_worked};
+
+        var postArg = {
+            auth: {
+                user: 'tschuy',
+                password: '$2a$10$6jHQo4XTceYyQ/SzgtdhleQqkuy2G27omuIR8M' +
+                          'PvSG8rwN4xyaF5W'
+            },
+        };
+
+        var requestOptions = {
+            url: baseUrl + 'times/1',
+            json: true
+        };
+
+        function copyJsonObject(obj) {
+            // This allows us to change object properties
+            // without effecting other tests
+            return JSON.parse(JSON.stringify(obj));
+        }
+
+        /*
+         * Okay so here's the deal.
+         * This endpoint has ~26 tests, which are honestly just 3 tests
+         * repeated 7 or 8 times (with a few exceptions).
+         * This function in theory gets rid of a lot of the repeated code in
+         * the tests.
+         * Without this function you would see this exact code pretty 26
+         * times over.
+         */
+        function sendDataToEndpoint(done,
+                                    postObj,
+                                    expectedResults,
+                                    error,
+                                    statusCode,
+                                    postBody){
+            postArg.object = copyJsonObject(postObj);
+            requestOptions.form = copyJsonObject(postArg);
+
+            // make a given post request
+            // check the error
+            // check the statusCode
+            // Also check the body of the request
+            request.post(requestOptions, function(err, res, body) {
+                expect(err).to.be.a(error);
+                expect(res.statusCode).to.equal(statusCode);
+
+                expect(body).to.deep.equal(expectedResults);
+
+                console.log(postBody);
+
+                // Always checks for valid get request
+                // err is always 'null'
+                // res.statusCode is always 200
+                // body always equals expectedresults
+                request.get(requestOptions.url, function(err, res, body) {
+                    expect(err).to.be.a('null');
+                    expect(res.statusCode).to.equal(200);
+
+                    expect(body).to.deep.equal(expectedResults);
+                    done();
+                });
+            });
+        }
+
+        // Tests all valid fields
+        it('succesfully patches time with valid duration, user, project,' +
+           ' activity notes, issue_uri, and date_worked', function(done) {
+
+            // This should have more data sent to sendDataToEndpoint
+            // This should have more data sent to sendDataToEndpoint
+            sendDataToEndpoint(done);
+/*
+            postArg.object = copyJsonObject(patchedTime);
+            requestOptions.form = copyJsonObject(postArg);
+
+            request.post(requestOptions, function(err, res, body) {
+                expect(err).to.be.a('null');
+                expect(res.statusCode).to.equal(200);
+
+                var exepctedResults = copyJsonObject(patchedTime);
+                expectedResults.id = originalTime.id;
+
+                expect(body).to.deep.equal(expectedResults);
+
+                checkEndpoint(done, expectedResults);
+            });
+*/
+        });
+
+        // Tests valid duration field
+        it('successfully patches time with valid duration', function(done) {
+            postObj = copyJsonObject(patchedTimeDuration);
+            expectedResults = copyJsonObject(originalTime);
+            expectedResults.duration = patchedTime.duration;
+
+            // This should have more data sent to sendDataToEndpoint
+            sendDataToEndpoint(done, postObj, epectedResults);
+        });
+
+        // Tests valid user field
+        // This test's functionality will be implemented at a later date
+        // (after the rest of the /time/:id functionality is implemented)
+        it('successfully patches time with valid user', function(done) {
+            done();
+        });
+
+        // Tests valid project field
+        it('successfully patches time with valid project', function(done) {
+            done();
+        });
+
+        // Tests valid activities field
+        it('successfully patches time with valid activities', function(done) {
+            done();
+        });
+
+        // Tests valid notes field
+        it('successfully patches time with valid notes', function(done) {
+            done();
+        });
+
+        // Tests valid issue_uri field
+        it('successfully patches time with valid issue_uri', function(done) {
+            done();
+        });
+
+        // Tests valid date_worked field
+        it('successfully patches time with valid date_worked', function(done) {
+            done();
+        });
+
+        // Tests all invalid fields
+        it('unsuccesfully patches time with invalid duration, user, project,' +
+           ' activity notes, issue_uri, and date_worked', function(done) {
+            done();
+        });
+
+        // Tests invalid duration field
+        it('unsuccessfully patches time with just invalid duration',
+           function(done) {
+            done();
+        });
+
+        // Tests invalid user field
+        // This test's functionality will be implemented at a later date
+        // (after the rest of the /time/:id functionality is implemented)
+        it('unsuccessfully patches time with just invalid user',
+           function(done) {
+            done();
+        });
+
+        // Tests invalid project field
+        it('unsuccessfully patches time with just invalid project',
+           function(done) {
+            done();
+        });
+
+        // Tests invalid activities field
+        it('unsuccessfully patches time with just invalid activites',
+           function(done) {
+            done();
+        });
+
+        // Tests invalid notes field
+        it('unsuccessfully patches time with just invalid notes',
+           function(done) {
+            done();
+        });
+
+        // Tests invalid issue_uri field
+        it('unsuccessfully patches time with just invalid issue_uri',
+           function(done) {
+            done();
+        });
+
+        // Tests invalid date_worked field
+        it('unsuccessfully patches time with just invalid date_worked',
+           function(done) {
+            done();
+        });
+
+        // Tests invalid key field
+        it('unsuccessfully patches time with just invalid key',
+           function(done) {
+            done();
+        });
+
+        // Tests all valid fields except invalid duration
+        it('unsuccessfully patches time with an invalid duration',
+            function(done) {
+            done();
+        });
+
+        // Tests all valid fields except invalid user
+        // This test's functionality will be implemented at a later date
+        // (after the rest of the /time/:id functionality is implemented)
+        it('unsuccessfully patches time with an invalid user',
+            function(done) {
+            done();
+        });
+
+        // Tests all valid fields except invalid project
+        it('unsuccessfully patches time with an invalid project',
+            function(done) {
+            done();
+        });
+
+        // Tests all valid fields except invalid activities
+        it('unsuccessfully patches time with an invalid activities',
+            function(done) {
+            done();
+        });
+
+        // Tests all valid fields except invalid notes
+        it('unsuccessfully patches time with an invalid notes',
+            function(done) {
+            done();
+        });
+
+        // Tests all valid fields except invalid issue_uri
+        it('unsuccessfully patches time with an invalid issue_uri',
+            function(done) {
+            done();
+        });
+
+        // Tests all valid fields except invalid date_worked
+        it('unsuccessfully patches time with an invalid date_worked',
+            function(done) {
+            done();
+        });
+
+        // Tests all valid fields except invalid key
+        it('unsuccessfully patches time with an invalid key',
+            function(done) {
+            done();
+        });
+    });
 };
